aebe89fcca17540f20e908c2cf1c0bfe018e2049
Torben <tkn@zuehlke.com>
2013-01-20 22:56:03 +0100

    notwendige Änderungen um das myapp::web Rezept lauffähig zu machen

--- myapp-1/myapp/recipes/web.rb
+++ myapp-1/myapp/recipes/web.rb
@@ -13,26 +13,38 @@ include_recipe "apt"
 # dann Tomcat installieren
 include_recipe "tomcat"
 
+db_host = node['myapp']['web']['db_host']
+db_name = node['myapp']['db']['dbname']
+db_user = node['myapp']['db']['username']
+db_pass = node['myapp']['db']['password']
+
 # my-app deployen
 application "my-app" do
   path "/usr/local/my-app"
-  repository "http://33.33.13.10/myapp-1.0.war"
+  owner node["tomcat"]["user"]
+  group node["tomcat"]["group"]
+
+  # wir nehmen das Yabe Beispiel vom Play! Framework:
+  # http://www.playframework.org/documentation/1.0/samples
+  repository "http://dl.dropbox.com/u/13494216/yabe.war"
   revision "1.0"
+  strategy :java_remote_file
 
   java_webapp do
     database do
       driver 'org.postgresql.Driver'
       adapter 'postgresql'
-      database node['myapp']['db']['dbname']
-      host node['myapp']['web']['db_host']
+      database db_name
+      host db_host
       port 5432
-      username node['myapp']['db']['username']
-      password node['myapp']['db']['password']
+      username db_user
+      password db_pass
+      max_active 1
+      max_idle 2
+      max_wait 3
     end
   end
 
   tomcat
 end
-
-
 
